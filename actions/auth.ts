28451b4666970e53dbc8445d96ac682dc230009a
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-12-28 11:51:49 +0530

    correct import

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import {signIn, signOut} from "@/auth"
+import {signIn, signOut} from "@/lib/auth"
 import { revalidatePath } from "next/cache"
 
 export const login = async (provider: string) => {
